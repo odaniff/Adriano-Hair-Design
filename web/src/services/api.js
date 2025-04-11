72802163333278b78afd7947cb6a1c6db18056f0
odaniff <daniels2625@gmail.com>
2025-04-11 01:20:39 -0300

    Update api.js

--- web/src/services/api.js
+++ web/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'http://localhost:8000',
-    baseURL: 'adriano-hair-design-production.up.railway.app',
+    baseURL: 'https://adriano-hair-design-production.up.railway.app',
 });
 
 export default api;
